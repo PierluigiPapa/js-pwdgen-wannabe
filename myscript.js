0ce02138f1a165f6eca34da3a88d501f00bd6dae
PierluigiPapa <pierluigipapa9@gmail.com>
2023-12-21 16:20:55 +0100

    feat: add js

---
+++ myscript.js
@@ -0,0 +1,10 @@
+// variabili
+
+let nome= prompt("Come ti chiami:", "nome");
+let cognome= prompt("Qual è il tuo cognome:", "cognome");
+let colore= prompt("Quale è il tuo colore preferito: ", "colorepreferito");
+let numero= prompt("Quale numero indossava Michael Jordan", "23")
+
+console.log(nome + '' + cognome + '' + colore + '' + numero);
+document.writeln(nome + '' + cognome + '' + colore + '' + numero);
+
